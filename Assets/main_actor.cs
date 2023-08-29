2dec87f9a72f2cd07c0f1edc4e4ef6e88b0e47cb
vm6u6 <ih.hsu1234@gmail.com>
2023-08-30 01:46:43 +0800

    Fix: power

--- Assets/main_actor.cs
+++ Assets/main_actor.cs
@@ -12,10 +12,10 @@ public class main_actor : MonoBehaviour
     [SerializeField] private GameObject power_bar;
     [SerializeField] private TextMeshProUGUI textMeshPro;
 
-    private float moveSpeed = 1f;              
+    private float moveSpeed = 1.5f;              
     private int max_floor = 0; 
     private int cnt_floor_certification = 0;   
-    private float downTime, upTime, pressTime = 0;
+    private int activateCount = 0;
     private float jumpForce = 5f;              
     private int moveDirection = 1;            
     private bool isJumping = false;           
@@ -23,7 +23,9 @@ public class main_actor : MonoBehaviour
     private SpriteRenderer spriteRenderer;    
     private Rigidbody2D rb;                   
     private float currentPressTime = 0;
-    private float timeOnGround = 0f;
+    private float timeOnGround_start = 0f;
+    private float timeOnGround_end = 0f;
+    private float overJumpingTime = 0.3f;
 
     
 
@@ -39,56 +41,44 @@ public class main_actor : MonoBehaviour
 
         // { Movement }_________________________________________________________________________
         float currentSpeed = moveSpeed;
-        if (Input.GetKey(KeyCode.Space))
-        {
-            currentSpeed *= 0.6f;
+        if (rb.velocity.y == 0f){
+            if (Input.GetKey(KeyCode.Space)){
+                currentSpeed *= 0.6f;
+            }
         }
+        
         float movement = moveDirection * currentSpeed * Time.deltaTime;
         transform.Translate(movement, 0f, 0f);
 
         // { Jumping }_________________________________________________________________________
-        if (Input.GetKeyDown(KeyCode.Space) && !isJumping && islanding)
-        {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f);
+        if (rb.velocity.y == 0f){
+            timeOnGround_end = Time.time;
+        
+            if (Input.GetKey(KeyCode.Space) && islanding){
+                currentPressTime += Time.deltaTime;
+                update_powerBar(currentPressTime);
+            }
+            if (timeOnGround_end - timeOnGround_start > overJumpingTime){
+                if (Input.GetKeyDown(KeyCode.Space) && !isJumping && islanding){
+                    isJumping = true;
+                }
 
-            if (hit.collider != null)
-            {
-                downTime = Time.time;
-                currentPressTime = 0;
-                isJumping = true;
+                if (Input.GetKeyUp(KeyCode.Space) && islanding){
+                    isJumping = false;
+                    StartJump();
+                }
             }
         }
-
-        if (Input.GetKey(KeyCode.Space) && islanding)
-        {
-            currentPressTime += Time.deltaTime;
-            update_powerBar(currentPressTime);
-        }
-
-        if (Input.GetKeyUp(KeyCode.Space) && islanding)
-        {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f);
-
-            if (hit.collider != null)
-            {
-                upTime = Time.time;
-                pressTime = upTime - downTime;
-                if (pressTime > 3)
-                    pressTime = 3;
-                isJumping = false;
-                StartJump();
-            }
-        }
-
         
         cnt_floor();
     }
 
     private void StartJump(){
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce + pressTime * 2f);
+        Debug.Log(activateCount);
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce + activateCount / 2);
         islanding = false;
-        pressTime = 0;
         jumpForce = 5f;
+        activateCount = 0;
     }
 
     private void OnCollisionEnter2D(Collision2D collision){
@@ -106,10 +96,11 @@ public class main_actor : MonoBehaviour
             collision.gameObject.CompareTag("acc_stair") && 
             rb.velocity.y == 0f)
         {
+            timeOnGround_start = Time.time;
             RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f);
-
             if (hit.collider != null)
             {
+                currentPressTime = 0;
                 isJumping = false;
                 islanding = true;
                 reset_powerBar(); 
@@ -124,13 +115,18 @@ public class main_actor : MonoBehaviour
             max_floor += 1;
             cnt_floor_certification += 3;
             textMeshPro.text = max_floor.ToString("D4") + "F";
+
+            moveSpeed += 0.2f;
         }
     }
 
     private void update_powerBar(float pressTime_)
     {
         int L = power_bar.transform.childCount;
-        int activateCount = Mathf.FloorToInt(pressTime_ / 0.25f);
+        activateCount = Mathf.FloorToInt(pressTime_ / 0.04f);
+        if (activateCount > 12){
+            activateCount = activateCount % 12;
+        }
 
         for (int i = 0; i < L; i++)
         {
