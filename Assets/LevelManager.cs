21b22cb1fea1a2b0176ca61d95add2af4016e5a8
vm6u6 <ih.hsu1234@gmail.com>
2023-09-18 08:31:45 +0800

    Feat: level

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+    [SerializeField] private TextMeshProUGUI textMeshPro_level;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
